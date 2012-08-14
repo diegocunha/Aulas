e32641c66a1449b896772d41fcc9479fae77f560
631120072 <631120072@Sala603-14.educcbx.senacrs.com.br>
2012-08-13 21:47:41 -0300

    commit 01

---
+++ entregas/aula02/src/main/java/br/com/senac/diegocunha/aula02/Pessoa.java
@@ -0,0 +1,17 @@
+package br.com.senac.diegocunha.aula02;
+
+public class Pessoa {
+	
+	private String nome = null;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
