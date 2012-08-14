e4915f03d66ffc3a42c94e0dcfa65ba8c7864159
631120072 <631120072@Sala603-14.educcbx.senacrs.com.br>
2012-08-13 22:14:25 -0300

    next

---
+++ entregas/aula02/src/main/java/br/com/senac/diegocunha/aula02/Funcionario.java
@@ -0,0 +1,15 @@
+package br.com.senac.diegocunha.aula02;
+
+public class Funcionario  extends Pessoa {   /// extends = herda da classe Pessoa
+	
+	private Integer id = null;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+		
+}
